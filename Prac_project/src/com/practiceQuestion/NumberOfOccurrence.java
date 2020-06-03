9b07259af17e4e9b255b1cf8a235590d8916dae6
mbiswas <miltonbiswas012@gmail.com>
2020-06-03 21:26:38 +0530

    NumberOfOccurrence

--- Prac_project/src/com/practiceQuestion/NumberOfOccurrence.java
+++ Prac_project/src/com/practiceQuestion/NumberOfOccurrence.java
@@ -1,26 +1,32 @@
 package com.practiceQuestion;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class NumberOfOccurrence {
 	
-	public static void numberOfOccurrence(String str) {
+	public static void numberOfOccurrence(String inputString) {
 		
-		HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-		for(int i=0; i<str.length(); i++) {
-			
-			char key=str.charAt(i);
-			
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key)+1);
-			}
-			else {
-				map.put(key, 1);
-			}
-		}
-		
-		System.out.println(map);
+		char[] strArray = inputString.replaceAll("\\s+", "").toCharArray(); 
+		System.out.println(Arrays.toString(strArray));
+		LinkedHashMap<Character, Integer> charCountMap=new LinkedHashMap<Character, Integer>();		
+		for (char c : strArray) { 
+            if (charCountMap.containsKey(c)) { 
+  
+                // If char is present in charCountMap, 
+                // incrementing it's count by 1 
+                charCountMap.put(c, charCountMap.get(c) + 1); 
+            } 
+            else { 
+  
+                // If char is not present in charCountMap, 
+                // putting this char to charCountMap with 1 as it's value 
+                charCountMap.put(c, 1); 
+            } 
+        } 
+		System.out.println(charCountMap);
 	} 
 	
 	public static void main(String[] args) {
